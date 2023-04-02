dd42f23ff171461d7475dd207686d3d49f977ffa
Viktor Modroczký <v.modroczky@gmail.com>
2023-04-02 18:30:17 +0200

    the script now converts raw measurements to SI units and labels them with an anxiety level

--- src/scripts/wesad.py
+++ src/scripts/wesad.py
@@ -9,6 +9,9 @@ from argparse import ArgumentParser
 import logging
 from pathlib import Path
 import re
+from io import TextIOWrapper
+from typing import Any, Callable
+import json
 from utils.scales import AnxietyLevel, Feeling, LikertScale4
 
 type = {
@@ -80,20 +83,89 @@ def get_anxiety_level(score: int) -> AnxietyLevel:
     return AnxietyLevel.High
 
 
-def label_anxiety(wesad_path: str) -> None:
+def get_metadata(header: list[str]) -> tuple[str, list[str]]:
     """
-    Function for labeling anxiety using the shortened
-    STAI questionnaire responses in the WESAD dataset.
+    Returns the metadata of a given header.
 
-    The function will create a new CSV file for each subject
-    with the following columns:
-        - condition: condition in the experiment
-        - start: start time of the condition
-        - end: end time of the condition
-        - stai_score: shortened STAI score
-        - anxiety_level: anxiety level based on the shortened STAI score
+    :param header: Header of the respiban file.
+    :return: Tuple of the start time and the sensors.
+    """
+    meta: dict[str, Any] = json.loads(header[1].split("#", maxsplit=1)[1])
+    metadata: dict[str, Any] = meta[list(meta.keys())[0]]
+    return metadata["time"], metadata["sensor"]
+
+
+def read_respiban_df(
+    file: TextIOWrapper,
+    sensors: list[str],
+    conversions: dict[str, Callable[[int], float]],
+) -> pd.DataFrame:
+    """
+    Returns a DataFrame of the respiban file with the desired converted columns.
+
+    :param file: File object of the respiban file.
+    :param sensors: List of sensors in the respiban file.
+    :param conversions: Dictionary of conversions of the raw data to the desired units.
+    :return: DataFrame of the respiban file with the desired converted columns.
+    """
+    df = (
+        pd.read_csv(file, sep="\t", header=None, index_col=0)
+        .dropna(axis=1)
+        .drop(1, axis=1)
+    )
+    df.columns = sensors
+    df.index.name = None
+    columns = list(conversions.keys())
+    result_df = pd.DataFrame(columns=columns)
+    for column in columns:
+        result_df[column] = df[column].apply(conversions[column])
+    return result_df
+
+
+def read_subject(
+    path: str,
+    subject: str,
+    conversions: dict[str, Callable[[int], float]],
+) -> tuple[str, pd.DataFrame]:
+    """
+    Returns the start time and DataFrame of the respiban file of a given subject.
 
     :param path: Path to the WESAD dataset.
+    :param subject: Subject ID.
+    :param conversions: Dictionary of conversions of the raw data to the desired units.
+    :return: Tuple of the start time and the DataFrame of the respiban file.
+    """
+    with open(os.path.join(path, subject, f"{subject}_respiban.txt")) as f:
+        header = [f.readline() for _ in range(3)]
+        start_time, sensors = get_metadata(header)
+        df = read_respiban_df(f, sensors, conversions)
+    return start_time, df
+
+
+def process_wesad(
+    wesad_path: str,
+    conversions: dict[str, Callable[[int], float]],
+    sampling_rate: int,
+) -> None:
+    """
+    Function for labeling measurements with anxiety levels using the shortened
+    STAI questionnaire responses in the WESAD dataset.
+
+    The function will create a 6 new CSV files for each subject, one for the STAI
+    questionnaire results and one for each of the 5 conditions in the experiment:
+
+    - <subject>_STAI.csv: DataFrame with the following columns:
+        - condition: condition in the experiment
+        - start: offset from the start of the experiment where the condition starts
+        - end: offset from the start of the experiment where the condition ends
+        - stai_score: shortened STAI score
+        - anxiety_level: anxiety level based on the shortened STAI score
+    - <subject>_<condition>_<anxiety_level>.csv: DataFrame with measurements
+    converted to the desired units, each in a separate column
+
+    :param path: Path to the WESAD dataset.
+    :param conversions: Dictionary of conversions of the raw data to the desired units.
+    :param sampling_rate: Sampling rate of the RespiBAN measurements.
     :return: None
     """
     if not os.path.exists(wesad_path):
@@ -106,7 +178,8 @@ def label_anxiety(wesad_path: str) -> None:
         return
 
     for subject in subjects:
-        if subject == "STAI_data":
+        # Skip the processed folder and subjects with invalid names
+        if subject == "Processed":
             continue
         if re.fullmatch(r"^S[0-9]{1,2}$", subject) is None:
             logging.warning(f"Subject {subject} does not have a valid name.\n")
@@ -118,6 +191,8 @@ def label_anxiety(wesad_path: str) -> None:
             logging.warning(f"Subject {subject} does not have a questionnaire file.\n")
             continue
 
+        # Read questionnaire and get condition, STAI score,
+        # START and END and determine the anxiety level.
         questionnaire = pd.read_csv(
             questionnaire_path,
             sep=";",
@@ -134,10 +209,10 @@ def label_anxiety(wesad_path: str) -> None:
         stai_score = stai.iloc[:, 1:].astype(int).apply(get_score, axis=1)
 
         start = questionnaire[questionnaire[0] == "START"].dropna(axis=1)
-        start = start.iloc[:, 1 : len(condition) + 1].astype(float)
+        start = start.iloc[:, 1 : len(condition) + 1].astype(str)
 
         end = questionnaire[questionnaire[0] == "END"].dropna(axis=1)
-        end = end.iloc[:, 1 : len(condition) + 1].astype(float)
+        end = end.iloc[:, 1 : len(condition) + 1].astype(str)
 
         formatted = pd.DataFrame(
             {
@@ -149,20 +224,93 @@ def label_anxiety(wesad_path: str) -> None:
         )
         formatted["anxiety_level"] = formatted["stai_score"].apply(get_anxiety_level)
 
-        output_path = os.path.join(wesad_path, "STAI_data")
+        output_path = os.path.join(wesad_path, "Processed", subject)
         Path(output_path).mkdir(parents=True, exist_ok=True)
 
-        formatted.to_csv(os.path.join(output_path, f"{subject}_STAI.csv"), index=False)
+        # Save STAI questionnaire results to CSV file.
+        stai_outut = os.path.join(output_path, f"{subject}_STAI.csv")
+        formatted.to_csv(stai_outut, index=False)
 
         logging.info(f"Anxiety levels: {formatted['anxiety_level'].ravel()}")
-        logging.info(f"Saved to '{os.path.join(output_path, subject)}_STAI.csv'\n")
+        logging.info(f"Saved to '{stai_outut}'")
+
+        # Read respiban file and get the start time and DataFrame of measurements.
+        _, respiban = read_subject(wesad_path, subject, conversions)
+
+        # Seperate measurements into conditions with anxiety level labels.
+        for _, row in formatted.iterrows():
+            current_condition: str = row["condition"]
+            current_start: str = row["start"]
+            current_end: str = row["end"]
+            current_anxiety_level: int = row["anxiety_level"]
+
+            # Calculate interval of samples for the condition.
+            # The start and end times have a format of 'minutes.seconds'.
+            start_split = current_start.split(".")
+            start_seconds = int(start_split[0]) * 60 + (
+                int(start_split[1]) if len(start_split) > 1 else 0
+            )
+
+            end_split = current_end.split(".")
+            end_seconds = int(end_split[0]) * 60 + (
+                int(end_split[1]) if len(end_split) > 1 else 0
+            )
+
+            # Start and end times are multiplied by the sampling rate to get the
+            # interval of samples for the condition.
+            interval = (start_seconds * sampling_rate, end_seconds * sampling_rate)
+
+            condition_df = respiban.iloc[interval[0] : interval[1] + 1]
+            current_condition = current_condition.replace(" ", "")
+
+            condition_output = os.path.join(
+                output_path,
+                f"{subject}_{current_condition}_{current_anxiety_level}.csv",
+            )
+
+            condition_df.to_csv(
+                condition_output,
+                index=False,
+            )
+
+            logging.info(
+                f"Saved {current_condition} with anxiety level {current_anxiety_level} to '{condition_output}'"
+            )
+
+
+def create_cli() -> ArgumentParser:
+    parser = ArgumentParser(
+        description="Script for labeling measurements with anxiety levels using the shortened STAI questionnaire responses in the WESAD dataset."
+    )
+    parser.add_argument(
+        "path",
+        type=str,
+        help="Path to the WESAD dataset",
+    )
+    return parser
+
+
+def main() -> None:
+    cli = create_cli()
+    args = cli.parse_args()
+
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
+
+    sampling_rate = 700  # Hz - sampling rate used for the RespiBAN device
+
+    # Conversion constants from the README file of the WESAD dataset
+    # used for converting the raw data to the desired units.
+    chan_bit = 2**16
+    vcc = 3
+
+    # Conversion functions for the raw data to the desired units.
+    si_conversion = {
+        "ECG": lambda x: (x / chan_bit - 0.5) * vcc,
+        "EDA": lambda x: ((x / chan_bit) * vcc) / 0.12,
+    }
+
+    process_wesad(args.path, si_conversion, sampling_rate)
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(
-        description="Script for labeling anxiety using the shortened STAI questionnaire responses in the WESAD dataset."
-    )
-    parser.add_argument("path", type=str, help="Path to the WESAD dataset")
-    args = parser.parse_args()
-    logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
-    label_anxiety(args.path)
+    main()
